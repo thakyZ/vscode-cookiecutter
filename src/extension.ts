34bd394669f4082f45d2e392d58e89785bec515a
thakyZ <thakyz@outlook.com>
2023-02-15 11:21:53 -0800

    Rebuilt extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,44 +1,73 @@
-// The module 'vscode' contains the VS Code extensibility API
+// The module "vscode" contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-'use strict';
-var vscode = require('vscode');
-const fs = require('fs');
-const path = require('path');
-const yaml = require('js-yaml');
+import * as vscode from "vscode";
+import { Position, TextDocument, Uri } from "vscode";
+import { promises as fs, readdirSync, lstatSync } from "fs";
+import path = require("path");
+import yaml = require("js-yaml");
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
-    var Cookiecutter = vscode.commands.registerCommand('extension.cookiecutter', function (param) {
-        let term = vscode.Terminal;
-
-        const ccConf = path.join(process.env.HOME, ".cookiecutterrc");
-        var tmpDir = yaml.safeLoad(fs.readFileSync(ccConf, 'utf8')).cookiecutters_dir;
-        var re = /^~/;
-        const ccDir = tmpDir.replace(re, process.env.HOME);
-        const items = (srcPath) => {
-            return fs.readdirSync(srcPath).filter(file => fs.lstatSync(path.join(srcPath, file)).isDirectory())
+    const Cookiecutter = vscode.commands.registerCommand("extension.cookiecutter", async (param) => {
+        let openEditor;
+        if (vscode.window.activeTextEditor.document.uri.scheme === "SEARCH_EDITOR_SCHEME") {
+            vscode.window.showInformationMessage("You can't use cookiecutter in Search Editor");
+            return;
         }
-        const template = (ccDir) => {
+        if (param === undefined && vscode.window.activeTextEditor !== undefined) {
+            if (vscode.window.activeTextEditor.document.uri.scheme === "file") {
+                openEditor = path.dirname(vscode.window.activeTextEditor.document.uri.fsPath);
+            }
+        } else {
+            openEditor = param.fsPath;
+        }
+
+        const isWin = process.platform === "win32";
+        let term = vscode.window.activeTerminal;
+
+        const ccConf = isWin === true ? path.join(process.env.USERPROFILE, ".cookiecutterrc") : path.join(process.env.HOME, ".cookiecutterrc");
+        const cookieCuttersRC = await fs.readFile(ccConf, "utf8");
+        const tmpDir = yaml.load(cookieCuttersRC).cookiecutters_dir;
+        const re = /^~/g;
+        const ccDir = tmpDir.replace(re, isWin === true ? process.env.USERPROFILE : process.env.HOME);
+        const items = srcPath => {
+            return readdirSync(srcPath).filter(file => lstatSync(path.join(srcPath, file)).isDirectory())
+        }
+        const template = ccDir => {
             return vscode.window.showQuickPick(items(ccDir), {
                 placeHolder: "Choosing a template.",
                 ignoreFocusOut: true,
             });
         };
-        template(ccDir).then(selection => {
+        await template(ccDir).then(selection => {
             // the user canceled the selection
-            if (selection === false) {
+            if (selection === undefined || selection === "") {
+                return;
+            }
+            let termProfile = "";
+
+            try {
+                termProfile = isWin === true ? vscode.workspace.getConfiguration("terminal").get("external.windowsExec") : vscode.workspace.getConfiguration("terminal").get("external.linuxExec");
+
+                if (termProfile === undefined) {
+                    vscode.window.showErrorMessage(`Config option terminal.external.${isWin === true ? "windowsExec" : "linuxExec"} is not defined.`);
+                    return;
+                }
+            } catch (error) {
+                vscode.window.showErrorMessage(`Config option terminal.external.${isWin === true ? "windowsExec" : "linuxExec"} is not defined.`);
                 return;
             }
 
             // the user selected some item. You could use `selection.name` too
-            var command = `cookiecutter -o ${param.path} ${selection}`;
+            const command = `cookiecutter -o ${openEditor} ${selection}`;
 
             if (vscode.window.terminals.length < 1) {
-                term = vscode.window.createTerminal("bash");
+
+                term = vscode.window.createTerminal(termProfile);
             } else {
                 term = vscode.window.activeTerminal;
-            }
+            } 1
             term.show();
             term.sendText(command);
         });
@@ -51,7 +80,7 @@ function activate(context) {
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() { }
 exports.deactivate = deactivate;
 
 
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+//@ts-check
+
+"use strict";
+
+const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
+const webpack = require("webpack");
+
+
+/**@type {import("webpack").Configuration}*/
+const config = {
+    entry: "./src/extension.ts",
+    optimization: {
+        minimizer: [new TerserPlugin({
+            parallel: true,
+            terserOptions: {
+                ecma: 2019,
+                keep_classnames: false,
+                mangle: true,
+                module: true
+            }
+        })],
+    },
+
+    devtool: "source-map",
+    externals: {
+        vscode: "commonjs vscode" // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack"ed, 📖 -> https://webpack.js.org/configuration/externals/
+    },
+    resolve: { // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
+        extensions: [".ts", ".js"]
+    },
+    module: {
+        rules: [{
+            test: /\.ts$/,
+            exclude: /node_modules/,
+            use: [{
+                loader: "ts-loader",
+            }]
+        }]
+    },
+};
+
+const nodeConfig = {
+    ...config,
+    target: "node",
+    output: { // the bundle is stored in the "dist" folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
+        path: path.resolve(__dirname, "dist"),
+        filename: "extension-node.js",
+        libraryTarget: "commonjs2",
+        devtoolModuleFilenameTemplate: "../[resource-path]",
+    },
+}
+
+module.exports = [nodeConfig];
